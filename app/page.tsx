2fd9acb4fb35204d91202ff27fe77e3f285177ae
alief wafiq ramadhan <aliefwafiq2@gmail.com>
2025-08-29 16:10:57 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import FontAwesomeConfig from "@/app/fontawesome";
+import Link from "next/link";
 import "@/app/globals.css";
 
 export default function RootLayout() {
@@ -8,7 +9,20 @@ export default function RootLayout() {
         <FontAwesomeConfig />
       </head>
       <body className={`antialiased`}>
-            <main className="px-6">landing page</main>
+        <div className="flex items-center justify-center w-full h-screen gap-4">
+          <Link
+            className="bg-blue-400 p-4 text-white flex items-center w-24 rounded-lg"
+            href="/register"
+          >
+            Register
+          </Link>
+          <Link
+            className="bg-blue-400 p-4 text-white flex items-center w-24 rounded-lg"
+            href="/login"
+          >
+            Log in
+          </Link>
+        </div>
       </body>
     </html>
   );
