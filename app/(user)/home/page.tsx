285cffbf5930256648657b867ca4d1a0f235b3bd
alief wafiq ramadhan <aliefwafiq2@gmail.com>
2025-09-26 09:17:58 +0700

    update

--- app/(user)/home/page.tsx
+++ app/(user)/home/page.tsx
@@ -11,7 +11,7 @@ import ListGames from "@/components/ListGames";
 
 export const dynamic = "force-dynamic";
 
-export default async function Home({
+export default function Home({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
