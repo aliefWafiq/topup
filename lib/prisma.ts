ee0f550e27d59a7138881637fafc549a941c5bec
alief wafiq ramadhan <aliefwafiq2@gmail.com>
2025-11-20 21:51:17 +0700

    update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,11 @@
-// lib/prisma.ts  atau  app/lib/prisma.ts
-
 import { PrismaClient } from '@prisma/client'
 
 declare global {
-  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined
 }
 
-// Ini yang paling benar & aman untuk Next.js App Router + Pages Router + Vercel
 const prisma = global.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma
-}
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
 
 export default prisma
