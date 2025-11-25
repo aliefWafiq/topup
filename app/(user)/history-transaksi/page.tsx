9a2db27bf33794d7ced3e701cc69c6429e3560fc
alief wafiq ramadhan <aliefwafiq2@gmail.com>
2025-11-25 07:54:50 +0700

    update

--- app/(user)/history-transaksi/page.tsx
+++ app/(user)/history-transaksi/page.tsx
@@ -9,7 +9,7 @@ const HistoryTransaksiPage = () => {
   return (
     <div className="min-h-screen flex justify-center py-32">
         <div className="w-full md:w-auto">
-            <h1 className="text-2xl font-bold">Histori Transaksi</h1>
+            <h1 className="text-2xl font-bold text-white">Histori Transaksi</h1>
             <HistoryTransaksiTable />
         </div>
     </div>
